3ab93702eba6060d1695c10771a97c127b8d2030
Richard <ngangarichard335@gmail.com>
2024-03-30 14:04:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+let names = ["Charlie", "Samip", "Ali"]
+function writeCards(names,event){
+    let messages = []
+    for (let i = 0; i < names.length; i++)
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    return messages
+}
+console.log(writeCards(names, "birthday"))
+
+function countDown(num) {
+    let count = num; 
+    while (count >= 0) {
+        console.log(count--); 
+    }
+}
+
+countDown(10);
+
+
+
+
